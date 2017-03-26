54167dcae11fbeb36455595315744187dcb0137e
Hann-B <hlbernard124@gmail.com>
2017-03-26 19:18:25 -0400

    Add files via upload
    I used powershell to commit and push, but when i refreshed the github page it was not updated so just in case here is another copy

--- BlackjackGame/Deck.cs
+++ BlackjackGame/Deck.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlackjackGame
 {
-        public enum Suit
-        {
-            Hearts,
-            Clubs,
-            Diamonds,
-            Spades
-        }
-    
+    class Deck
+    {
+    }
 }
